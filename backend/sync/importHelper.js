bc006e1a0ad6f1e0f2e732de41945b8327f1c31c
Calvin Secrest <dev@calvinsecrest.com>
2025-04-06 21:02:20 -0400

    Create importHelper.js

---
+++ backend/sync/importHelper.js
@@ -0,0 +1,30 @@
+const Listing = require("../models/listing");
+const Transaction = require("../models/transaction");
+const User = require("../models/user");
+
+async function importFederatedData(data) {
+  for (const listing of data.listings || []) {
+    await Listing.updateOne(
+      { _id: listing._id },
+      { $set: listing },
+      { upsert: true }
+    );
+  }
+  for (const transaction of data.transactions || []) {
+    await Transaction.updateOne(
+      { _id: transaction._id },
+      { $set: transaction },
+      { upsert: true }
+    );
+  }
+  for (const user of data.users || []) {
+    await User.updateOne(
+      { _id: user._id },
+      { $set: user },
+      { upsert: true }
+    );
+  }
+  return { success: true };
+}
+
+module.exports = { importFederatedData };
