afad2a279403bb411567e33ea3d5c09af1fe15b4
Calvin Secrest <dev@calvinsecrest.com>
2025-05-31 00:38:54 -0400

    Update trendsRoutes.js

--- backend/trends/trendsRoutes.js
+++ backend/trends/trendsRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const Listing = require("../marketplace/models/listing");
+const Listing = require("../marketplace/models/listings");
 const Transaction = require("../models/transaction");
 
 // 1. Top Rated Producers
